3f25382e265dffc483fddad097437a4a092bf835
Vasil Todorov <vdtodorov93@gmail.com>
2017-04-16 00:53:18 +0300

    ex5: bias vs variance

---
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -0,0 +1,50 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   cost of using theta as the parameter for linear regression to fit the 
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost and gradient of regularized linear 
+%               regression for a particular choice of theta.
+%
+%               You should set J to the cost and grad to the gradient.
+%
+
+%size(X) = 12 2
+%size(y) = 12 1
+%size(theta) = 2 1
+%lambda = 1
+
+J_not_reg = sum((y - X * theta) .^ 2) / (2 * m);
+penalty = (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
+J = penalty + J_not_reg;
+
+grad = zeros(size(theta));
+
+nonreg_grad = (X' * (X * theta - y)) / m;
+grad = nonreg_grad + [0; (lambda / m) * (theta(2:end))];
+
+
+
+
+
+
+
+
+
+
+
+% =========================================================================
+
+grad = grad(:);
+
+end
